efcea01a4534c6b066c9d4c73a5c0baf55f0aa10
rafaelgscc <37811294+rafaelgscc@users.noreply.github.com>
2024-12-11 10:28:28 -0300

    adicionando c#

---
+++ POO/csharp/Cao.cs
@@ -0,0 +1,14 @@
+namespace csharp
+{
+  class Cao
+  {
+    public string nome;
+    public string cor;
+    public int idade;
+    public float peso;
+
+    public void Andar(){
+        Console.WriteLine("Estou Andando!!");
+    } 
+  }
+}
---
+++ POO/csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+//csharp é muito parecido com java, porém é necessário criar um espaço usando "dotnet new console -o app" 
+namespace csharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Cao cachorro = new Cao();
+        cachorro.nome = "billy";
+        Console.WriteLine(cachorro.nome);
+    }
+  }
+}
