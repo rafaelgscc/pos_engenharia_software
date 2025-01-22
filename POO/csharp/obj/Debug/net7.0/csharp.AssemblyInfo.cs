be528e504257400636bec9536271929768c8023f
rafaelgscc <37811294+rafaelgscc@users.noreply.github.com>
2025-01-22 14:25:41 -0300

    autalizado alguns caches

--- POO/csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
+++ POO/csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e46cd5b982ecc296fbf0bfab7f8f2ee7c79273e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efbc1e3f07d3a691b6431403217ab443d8c3e056")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
